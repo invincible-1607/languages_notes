68a838435e8deb0d00f228dfba66d5c974ac43f3
invincible-1607 <95834364+invincible-1607@users.noreply.github.com>
2021-12-09 07:34:48 +0530

    Add files via upload

---
+++ PYTHON/pycharm python/python assingents/Looping Statement-1.py
@@ -0,0 +1,33 @@
+# n=int(input("Enter The Value Of N: "))
+# for i in range(n,0,-1):              #row
+#     for j in range(1,i+1):          #col
+#         print(j,end="")
+#     print()
+
+
+# n=int(input("Enter Number Of Rows: "))
+# for i in range(n,0,-1):
+#     for j in range(1,i+1):
+#         print(i,end="")
+#     print()
+
+# n=int(input("Enter Number Of Rows: "))
+# for i in range(1,n+1,-1):
+#     for j in range(1,i+1):
+#         print(i,end="")
+#     print()
+
+# n=int(input("Enter Number Of Rows: "))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if((i==1 and j==1) or (i==1 and j==5) or (i==5 and j==1) or (i==5 and j==5)):
+#             print(" +",end="")
+#         else:
+#             print(" *",end="")
+#     print()
+
+# count=1
+# n=int(input("Enter Your Ending Number: "))
+# while(count<=n):
+#     print(count*count)
+#     count+=1
